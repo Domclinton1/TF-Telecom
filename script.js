ab46cfe277f145f509f8cf7cd6535773da6bc6fc
Clinton <clinton.admpro29@gmail.com>
2024-10-21 15:30:11 -0300

    Adcionando planos de internet

--- script.js
+++ script.js
@@ -83,6 +83,26 @@ function wpp() {
     var click = document.querySelectorAll('.cta')
     window.open("https://wa.me/5531997715545?text=Ola,%20quero%20fazer%20um%20orçamento%20sobre um projeto...")
 }
+function plano1() {
+    var click = document.querySelectorAll('.cta')
+    window.open("https://wa.me/5531997715545?text=Ola,%20quero%20contratar%20o%20plano%20de%20internet%20de 420 MEGA...")
+}
+function plano2() {
+    var click = document.querySelectorAll('.cta')
+    window.open("https://wa.me/5531997715545?text=Ola,%20quero%20contratar%20o%20plano%20de%20internet%20de 750 MEGA...")
+}
+function plano3() {
+    var click = document.querySelectorAll('.cta')
+    window.open("https://wa.me/5531997715545?text=Ola,%20quero%20contratar%20o%20plano%20de%20internet%20de 780 MEGA...")
+}
+function plano4() {
+    var click = document.querySelectorAll('.cta')
+    window.open("https://wa.me/5531997715545?text=Ola,%20quero%20contratar%20o%20plano%20de%20internet%20de 850 MEGA...")
+}
+function plano5() {
+    var click = document.querySelectorAll('.cta')
+    window.open("https://wa.me/5531997715545?text=Ola,%20quero%20contratar%20o%20plano%20de%20internet%20de 1 GIGA...")
+}
 function wpp2() {
     var click = document.querySelectorAll('.cta')
     window.open("https://wa.me/5531992616974?text=Ola,%20quero%20fazer%20um%20orçamento%20sobre um projeto...")
@@ -99,3 +119,32 @@ function insta(){
     var click = document.querySelectorAll('insta')
     window.open("https://www.instagram.com/tftelecom_ltda/")
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const openModalButtons = document.querySelectorAll(".openModal");
+    const modals = document.querySelectorAll(".modal");
+    const closeButtons = document.querySelectorAll(".close");
+
+    openModalButtons.forEach(function(button){
+        button.addEventListener("click", function(){
+            const modalId = button.getAttribute("data-modal-id");
+            const modal = document.getElementById(modalId);
+            modal.style.display = "block";
+        });
+    });
+
+    closeButtons.forEach(function(button) {
+        button.addEventListener("click", function(){
+            const modal = button.closest(".modal");
+            modal.style.display = "none";
+        });
+    });
+
+    window.addEventListener("click", function(event) {
+        modals.forEach(function(modal) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        });
+    });
+});
